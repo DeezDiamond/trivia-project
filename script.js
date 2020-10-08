675aea71d2c57c6b332269b106633b4a1d96c21a
Dee <71417512+DeezDiamond@users.noreply.github.com>
2020-10-08 13:32:03 -0600

    stinky validator doesn't like my href buttons

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const getHelp = document.querySelector("#begin")
-const questOne = document.querySelector("#questionOne")
-const questTwo = document.querySelector("#questionTwo")
-const questThree = document.querySelector("#questionThree")
+// ---- BUTTON FUNCTIONALITY ---- //
+// ---- We are using the Button ID's ---- //
+// ---- The function should contain the card Div ID name ---- //
+
+const startGame = document.querySelector("#begin")
+const questOne = document.querySelector('#goToOne');
+const questTwo = document.querySelector("#goToTwo")
+const questThree = document.querySelector("#goToThree")
 const newGame = document.querySelector('#instructions')
 
 const begin = () => {
     instructions.style.display = "block"
 }
-getHelp.addEventListener("click", begin)
+startGame.addEventListener("click", begin)
+
+const goToOne = () => {
+    questionOne.style.display = "block"
+}
+questOne.addEventListener("click", goToOne)
+
+const goToTwo = () => {
+	questionTwo.style.display = 'block';
+};
+questTwo.addEventListener('click', goToTwo);
+
+// --- Need to get the cards to overlap on top of each other upon clicking a new one -- //
