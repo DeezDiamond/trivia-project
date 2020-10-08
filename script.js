1dddc0ee1436b50e0b0d0f3f0b23c4e3d9e37cd5
Dee <71417512+DeezDiamond@users.noreply.github.com>
2020-10-08 15:33:06 -0600

    bah

--- script.js
+++ script.js
@@ -2,35 +2,35 @@
 // ---- We are using the Button ID's ---- //
 // ---- The function should contain the card Div ID name ---- //
 
-const startGame = document.querySelector("#begin")
+const startGame = document.querySelector('#begin');
 const questOne = document.querySelector('#goToOne');
-const questTwo = document.querySelector("#goToTwo")
-const questThree = document.querySelector("#goToThree")
-const newGame = document.querySelector('#restart')
+const questTwo = document.querySelector('#goToTwo');
+const questThree = document.querySelector('#goToThree');
+const newGame = document.querySelector('#restart');
 
 const begin = () => {
-    instructions.style.display = "block"
-    intro.style.display = "none"
-}
-startGame.addEventListener("click", begin)
+	instructions.style.display = 'block';
+	intro.style.display = 'none';
+};
+startGame.addEventListener('click', begin);
 
 const goToOne = () => {
-    questionOne.style.display = "block"
-    instructions.style.display = "none"
-}
-questOne.addEventListener("click", goToOne)
+	questionOne.style.display = 'block';
+	instructions.style.display = 'none';
+};
+questOne.addEventListener('click', goToOne);
 
 const goToTwo = () => {
-    questionTwo.style.display = 'block';
-    questionOne.style.display = "none"
+	questionTwo.style.display = 'block';
+	questionOne.style.display = 'none';
 };
 questTwo.addEventListener('click', goToTwo);
 
 const goToThree = () => {
-    questionThree.style.display = "block"
-    questionTwo.style.display = "none"
-}
-questThree.addEventListener("click", goToThree)
+	questionThree.style.display = 'block';
+	questionTwo.style.display = 'none';
+};
+questThree.addEventListener('click', goToThree);
 
 const restart = () => {
 	intro.style.display = 'block';
@@ -38,27 +38,23 @@ const restart = () => {
 };
 newGame.addEventListener('click', restart);
 
+// // ---- SCORE CARD ---- //
+// const answers = document.querySelectorAll(".card");
+// let addPoints = 0
 
-// ---- SCORE CARD ---- //
-const answers = document.querySelectorAll(".card");
-let addPoints = 0
-
-// Why isn't this an event listener? The moment I put ()'s the event listener text changes to yellow and won't work.
-answers.addEventListener("click", (event) => {
-    console.log(event.target.dataset.increment);
-    if (event.target.dataset.increment == 10) {
-        addPoints +=10
-        addPointsScore.innerText = addPoints
-    } else if (event.target.dataset.increment == 0) {
-        addPoints += 0
-    };
-})
-// console.log(addPoints);
-
+// // // Why isn't this an event listener? The moment I put ()'s the event listener text changes to yellow and won't work.
+// answers.addEventListener("click", (event) => {
+//     console.log(event.target.dataset.increment);
+//     if (event.target.dataset.increment == 10) {
+//         addPoints +=10
+//         addPointsScore.innerText = addPoints
+//     } else if (event.target.dataset.increment == 0) {
+//         addPoints += 0
+//     };
+// })
 
 // --- Need to get the cards to overlap on top of each other upon clicking a new one -- //
 
-
-// --- use innHTML to clear out info in the current overlayed card and display if the player got the correct answer. 
+// --- use innHTML to clear out info in the current overlayed card and display if the player got the correct answer.
 // --- if answerPicked === answerA (congrats you did it!)
 // --- else answerPicked != answerA (Oops! The correct answer is `${answerA}`)
